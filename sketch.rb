ad70c6242995869a657f3c5a96be56215e9e54c9
mat <matthew.kelcey@gmail.com>
2009-05-21 21:42:55 +1000

    reduce memory usage by checking for count cutoff before building incommon map

--- sketch.rb
+++ sketch.rb
@@ -1,10 +1,10 @@
 p#!/usr/bin/env ruby
 
-raise "usage: sketch.rb HASH_SIZE SKETCH_SIZE CUTOFF" unless ARGV.length == 3
+raise "usage: sketch.rb HASH_SIZE SKETCH_SIZE CUTOFF_RATIO" unless ARGV.length == 3
 
 HASH_SIZE = ARGV[0].to_i
 SKETCH_SIZE ||= ARGV[1].to_i
-CUTOFF ||= ARGV[2].to_i
+CUTOFF_RATIO ||= ARGV[2].to_i
 
 require 'read_data'
 require 'set'
@@ -27,11 +27,11 @@ document_shingles = documents.collect do |id_text|
 end
 
 #puts "made shingles"
-
 #puts "document_ids #{document_ids.inspect}"
 #document_shingles.each { |ds| puts "document_shingle #{ds.inspect}" }
 
 sketches = Sketches.calculate_for document_shingles
+
 #puts "made sketches"
 #puts "sketches #{sketches.inspect}"
 
@@ -46,11 +46,13 @@ end
 sketches = nil
 x_to_i.reject! { |k,v| v.size==1 } # a hash that went to only one doc is useless
 
+#puts "made x_to_i"
 #puts "x_to_i.keys.size = #{x_to_i.keys.size}"
 #puts "x_to_i.values.sizes = #{x_to_i.collect{|k,v| v.size}}"
 #x_to_i.each {|kv| k,v=kv; puts "#{k} #{v.inspect}"}
 
 # count the number of pairs with any xi in common
+max_shingles_in_common = 0
 shingles_in_common = {} # (id1,id2) #common=3 & (id1,id3) #common=4 -> { id1 => { id2 => 3, id3 => 4} }
 x_to_i.keys.each do |x|
 #	puts "x #{x}"
@@ -65,50 +67,39 @@ x_to_i.keys.each do |x|
 #			puts "i #{i} j #{j} pi #{pi.inspect} pj #{pj.inspect}"
 
 			shingles_in_common[pi] ||= {}
-			shingles_in_common[pi][pj] ||= 0 # move to default?
-			shingles_in_common[pi][pj] += 1
+			shingles_in_common_pi = shingles_in_common[pi]
+			current = shingles_in_common_pi[pj]
+			current ||= 0
+			new_val = current + 1
+			max_shingles_in_common = new_val if new_val > max_shingles_in_common
+			shingles_in_common_pi[pj] = new_val
 
-#			shingles_in_common[pj] ||= {}
-#			shingles_in_common[pj][pi] ||= 0 # move to default?
-#			shingles_in_common[pj][pi] += 1
 		end
 	end
 end
 x_to_i = nil
 
-#puts "shingles in common.size #{shingles_in_common.size}"
+in_common_cutoff = max_shingles_in_common / CUTOFF_RATIO
+
+#puts "shingles in common.size #{shingles_in_common.size} max=#{max_shingles_in_common}"
 #shingles_in_common.keys.each { |k| puts "#{k} #{shingles_in_common[k].inspect}" }
 
 # convert from { id1=>{id2=>3, id3=>5}, id2=>{id3=>7}} to [ [id1,id2,3], [id1,id3,5], [id2,id3,7] ]
-MIN_COUNT = 1
 shingles_in_common_list = []
 shingles_in_common.keys.each do |k1|
 	shingles_in_common_with_k1 = shingles_in_common[k1]
 	shingles_in_common_with_k1.keys.each do |k2|
 		count = shingles_in_common_with_k1[k2]
-		shingles_in_common_list << [k1,k2,count] if count >= MIN_COUNT
+		shingles_in_common_list << [k1,k2,count] if count >= in_common_cutoff
 	end
 end
 shingles_in_common = nil
 
 #puts "shingles_in_common_list.size #{shingles_in_common_list.size}"
 
-# find max in common
-max = -1
-shingles_in_common_list.each do |ids_count|
-	count = ids_count[2]
-	max = count if count > max
-end
-
-#puts "max in common = #{max}"
-
 # calc and display jaccard coeffs ignoring those below cutoff
-#num_checked = 0
-count_cutoff = max / CUTOFF 
 shingles_in_common_list.each do |ids_count|
 	a,b,count = ids_count
-	next if count < count_cutoff
-	#num_checked += 1
 
 	id1,id2 = document_ids[a], document_ids[b]
 	d1,d2 = documents[id1], documents[id2]
@@ -118,4 +109,3 @@ shingles_in_common_list.each do |ids_count|
 #	puts "#{"-"*40}\n#{count} #{coeff} MMM\n\t#{a}\t#{d1}\n\t#{b}\t#{d2}"
 end
 
-#puts "num_checked #{num_checked} / total #{shingles_in_common_list.size}"
