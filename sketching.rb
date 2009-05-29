64a38abd93915a58726c1997982ca1a07a39fb99
mat <matthew.kelcey@gmail.com>
2009-05-29 20:23:55 +1000

    dont need to permutate, just use a number of different hash functions

--- sketching.rb
+++ sketching.rb
@@ -10,12 +10,7 @@ class Sketch
 	end
 
 	def apply_hash hash_fn
-		@hashes = @elems.each.collect { |elem| hash_fn.hash elem }
-	end
-
-	def apply_permutation permutation
-		@hashes = @hashes.collect { |h| permutation.permutate h }
-		@sketch << @hashes.min
+		@sketch << @elems.each.collect { |elem| hash_fn.hash elem }.min
 	end
 
 end
@@ -25,12 +20,9 @@ class Sketches
 	def self.calculate_for elems
 		sketches = elems.collect { |e| Sketch.new(e) }
 
-		hash_fn = UniversalHash.build
-		sketches.each { |s| s.apply_hash hash_fn }
-
 		SKETCH_SIZE.times do 
-			permutation = RandomPermutation.new hash_fn.max_value		
-			sketches.each { |s| s.apply_permutation permutation }
+			hash_fn = UniversalHash.build
+			sketches.each { |s| s.apply_hash hash_fn }
 		end
 		
 		sketches
