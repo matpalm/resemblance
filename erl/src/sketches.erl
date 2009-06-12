0a70a9ce3a856fd8dc09dc13eb59d68b4f1b6f0d
mat <matthew.kelcey@gmail.com>
2009-06-12 23:43:00 +1000

    code move from sketches to reducer

--- erl/src/sketches.erl
+++ erl/src/sketches.erl
@@ -24,29 +24,7 @@ slurp(File,Acc) ->
 	    slurp(File,[B|Acc])
     end.
   
-% given  ["1.0.gz","1.1.gz","2.0.gz","2.1.gz","2.2.gz"] 
-% return [ {"0",["1.0.gz","2.0.gz"]} , {"1",["1.1.gz","2.1.gz"]}, {2,["2.2.gz"]} ] 
-partition_filenames(Filenames) ->
-    FilesSplit = [ split(Filename) || Filename <- Filenames ],
-    lists:keysort(1,partition(FilesSplit)).
-    
-split(Filename) ->
-    Bits = re:split(Filename,"\\."),
-    [ binary_to_list(S) || S <- Bits ].
 
-partition(Files) ->
-    partition(Files, dict:new()).
-
-partition([],Partitions) ->
-    dict:to_list(Partitions);
-
-partition([[_,B,_]=File|Files],Partitions) ->
-    [A,B,C] = File,
-    Filename = A ++ "." ++ B ++ "." ++ C,
-    case dict:is_key(B,Partitions) of
-	true ->  partition(Files,dict:append(B,Filename,Partitions));
-	false -> partition(Files,dict:store(B,[Filename],Partitions))
-    end.
     
     
     
