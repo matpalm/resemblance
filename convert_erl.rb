07901f142b9bb8a85c3e8a12fe1d3003ed082cc9
mat <matthew.kelcey@gmail.com>
2009-06-30 23:09:16 +1000

    do shuffle in a single erl process which chains the three tasks together. first cut of the removal of freq terms

--- convert_erl.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# convert output from a run of erl sketching
-STDIN.each do |line|
-	next if line =~ /^stat/;
-	next if line =~ /main line/;
-	line.sub! 'sic ', ''
-  puts line
-end
-
--- erl/map_reduce.rb
+++ erl/map_reduce.rb
@@ -17,30 +17,35 @@ def run(command)
 	@last = now
 end
 
-NUM = ARGV[0] || "10"
-FILES = ARGV[1] || "10"
+NUM = ARGV.shift || "10"
+SRC_FILE = ARGV.shift || "../name_addr"
+FILES = ARGV.shift || "10"
 
-msg = "NUM=#{NUM} FILES=#{FILES}"
+msg = "NUM=#{NUM} SRC_FILE=#{SRC_FILE} FILES=#{FILES}"
 log msg
 
 # prepare data
 # { doc_id, "text of document here" }
-run "head -n #{NUM} ../name_addr | perl -plne'tr/A-Z/a-z/' | erl -noshell -pa ebin -s prepare -num_files #{FILES} -output_dir mr/01_prepared"
+run "head -n #{NUM} #{SRC_FILE} | perl -plne'tr/A-Z/a-z/' | erl -noshell -pa ebin -s prepare -num_files #{FILES} -output_dir mr/01_prepared"
 
 # map to { doc_id, [shingles] } 
-run "erl -noshell -pa ebin -s map_reduce_s -task shingler -input_dir mr/01_prepared -output_dir mr/02_id_to_shingles"
+run "erl -noshell -pa ebin -s map_reduce_s -task shingler -shingle_size 3 -input_dir mr/01_prepared -output_dir mr/02_id_to_shingles"
 
 # determine most frequent shingles
 # output { shingle, freq } 
+=begin
 run "erl -noshell -pa ebin -s map_reduce_s -task emit_values -input_dir mr/02_id_to_shingles -output_dir mr/02_2_shingle_to_1"
 run "erl -noshell -pa ebin -s shuffle -input_dir mr/02_2_shingle_to_1 -output_dir mr/02_3_shingle_to_1_shuffled"
 run "erl -noshell -pa ebin -s map_reduce_s -task sum -input_dir mr/02_3_shingle_to_1_shuffled -output_dir mr/02_4_shingle_freq"
 run "erl -noshell -pa ebin -s map_reduce_s -task top_N -num_to_keep 10 -input_dir mr/02_4_shingle_freq -output_dir mr/02_5_top_shingle_freq"
 run "erl -noshell -pa ebin -s reducer -task top_N -num_to_keep 10 -input_dir mr/02_5_top_shingle_freq -output_file mr/02_6_most_freq"
-
 # remove shingles that are in the most common set
 # input_files { doc_id, [shingles] } output_files { doc_id, [shingles] }
 run "erl -noshell -pa ebin -s map_reduce_s -task remove_common_shingles -common_file mr/02_6_most_freq -input_dir mr/02_id_to_shingles -output_dir mr/03_id_to_uncommon_shingles"
+=end
+
+# identity step when not removing freq shingles
+run "cp -r mr/02_id_to_shingles mr/03_id_to_uncommon_shingles"
 
 # map to { sketch_value, doc_id } 
 run "erl -noshell -pa ebin -s map_reduce_s -task sketcher -input_dir mr/03_id_to_uncommon_shingles -output_dir mr/04_sketches"
